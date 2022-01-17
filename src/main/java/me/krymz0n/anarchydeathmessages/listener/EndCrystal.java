9c61c4b78e5e59eee8adbdf8c0905d97079b3685
KRYMZ0N <jackgrimes05@yahoo.com>
2022-01-16 22:15:56 -0500

    Initial commit

---
+++ src/main/java/me/krymz0n/anarchydeathmessages/listener/EndCrystal.java
@@ -0,0 +1,24 @@
+package me.krymz0n.anarchydeathmessages.listener;
+
+import me.krymz0n.anarchydeathmessages.Main;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import java.util.Objects;
+
+public class EndCrystal implements Listener {
+    private final Main plugin;
+
+    public EndCrystal(Main plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent evt) {
+        if (Objects.requireNonNull(evt.getEntity().getLastDamageCause()).getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION) {
+            evt.setDeathMessage("fucker");
+        }
+    }
+}
